97f1aeeb6f81897bf8ec83805856923f3de60b61
haeny-dev <wjdeoaks92@gmail.com>
2021-04-05 17:06:18 +0900

    [Today I Learn] 프로그래머스 코딩테스트 연습 > 연습문제 : 가운데 글자 가져오기

---
+++ coding-test/programmers/src/kr/co/programmers/practice/GetMiddleLetter.java
@@ -0,0 +1,15 @@
+package kr.co.programmers.practice;
+
+public class GetMiddleLetter {
+
+    public static void main(String[] args) {
+        String s = "abcde";
+        System.out.println(new Solution().solution(s));
+    }
+
+    static class Solution {
+        public String solution(String s) {
+            return s.length()%2 == 0 ? s.substring(s.length()/2 - 1, s.length()/2 + 1) : String.valueOf(s.charAt(s.length()/2));
+        }
+    }
+}
