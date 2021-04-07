2c1cdc3fcf0a852b9ac764b0179cfc909a7e54df
haeny-dev <wjdeoaks92@gmail.com>
2021-04-07 17:21:08 +0900

    [Today I Learn] 프로그래머스 코딩테스트 연습 > 연습문제 : 나누어 떨어지는 숫자 배열

---
+++ coding-test/programmers/src/kr/co/programmers/practice/DivisibleArrayOfNumbers.java
@@ -0,0 +1,31 @@
+package kr.co.programmers.practice;
+
+import java.util.Arrays;
+import java.util.TreeSet;
+
+public class DivisibleArrayOfNumbers {
+
+    public static void main(String[] args) {
+        int[] arr = {5, 9, 7, 10};
+        int divisor = 5;
+        System.out.println(Arrays.toString(new Solution().solution(arr, divisor)));
+    }
+
+    static class Solution {
+        public int[] solution(int[] arr, int divisor) {
+            TreeSet<Integer> integerSet = new TreeSet<>();
+
+            for (int a : arr){
+                if ( a % divisor == 0){
+                    integerSet.add(a);
+                }
+            }
+
+            if(integerSet.isEmpty()){
+                integerSet.add(-1);
+            }
+
+            return integerSet.stream().mapToInt(x -> x).toArray();
+        }
+    }
+}
