1c9235a538d47f34e4fbed222dcad40bfff03ed3
haeny-dev <wjdeoaks92@gmail.com>
2021-04-17 22:36:18 +0900

    [Today I Learn] 프로그래머스 코딩테스트 연습 > 연습문제 : 시저 암호

---
+++ coding-test/programmers/src/kr/co/programmers/practice/CaesarCipher.java
@@ -0,0 +1,44 @@
+package kr.co.programmers.practice;
+
+public class CaesarCipher {
+
+    public static void main(String[] args) {
+        String s = "AB";
+        int n = 1;
+        System.out.println(new Solution().solution(s, n));
+    }
+
+    static class Solution {
+        public String solution(String s, int n) {
+            String answer = "";
+
+            for(int i = 0; i < s.length(); i++){
+                int asc = s.charAt(i);
+
+                if(asc == 32){
+                    answer += (char)asc;
+
+                } else if (asc >= 65 && asc <= 90){
+                    asc += n;
+
+                    if(asc > 90){
+                        asc = asc - 26;
+                    }
+
+                    answer += (char)asc;
+                } else if (asc >= 97 && asc <= 122){
+
+                    asc += n;
+
+                    if(asc > 122){
+                        asc = asc - 26;
+                    }
+
+                    answer += (char)asc;
+                }
+            }
+
+            return answer;
+        }
+    }
+}
