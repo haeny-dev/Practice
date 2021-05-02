e15a2c4130fef40c0cfb94df958f212236f16f76
haeny-dev <wjdeoaks92@gmail.com>
2021-05-02 23:18:46 +0900

    [Today I Learn] 프로그래머스 코딩테스트 연습 > 연습문제 : x만큼 간격이 있는 n개의 숫자

---
+++ coding-test/programmers/src/kr/co/programmers/practice/NSpaceByX.java
@@ -0,0 +1,25 @@
+package kr.co.programmers.practice;
+
+import java.util.Arrays;
+
+public class NSpaceByX {
+
+    public static void main(String[] args) {
+        int x = 2;
+        int n = 5;
+        System.out.println(Arrays.toString(new Solution().solution(x, n)));
+    }
+
+    static class Solution {
+        public long[] solution(int x, int n) {
+            long[] answer = new long[n];
+            long num = x;
+            for(int i = 0; i < n; i++){
+                answer[i] = num;
+                num += x;
+            }
+
+            return answer;
+        }
+    }
+}
