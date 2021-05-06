7a0474f36ecfb42352ba75174bd7bb057596ee39
haeny-dev <wjdeoaks92@gmail.com>
2021-05-06 16:54:29 +0900

    [Today I Learn] 패스트캠퍼스 > 한번에 끝내는 Java/Spring 웹 개발 마스터 초격차 패키지 Online > 자바 프로그래밍 ch07

---
+++ lecture/fastcampus/java-spring/src/main/java/kr/co/fastcampus/javaspring/java/ch07/DoubleTest.java
@@ -0,0 +1,13 @@
+package kr.co.fastcampus.javaspring.java.ch07;
+
+public class DoubleTest {
+
+    public static void main(String[] args) {
+
+        double dNum = 3.14;
+        float fNum = 3.14F;
+
+        System.out.println(dNum);
+        System.out.println(fNum);
+    }
+}
