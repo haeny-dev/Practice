1f632cb05161eeb3d580369d67157772783fee86
haeny-dev <wjdeoaks92@gmail.com>
2021-05-10 19:18:25 +0900

    [Today I Learn] 패스트캠퍼스 > 한번에 끝내는 Java/Spring 웹 개발 마스터 초격차 패키지 Online > 자바 프로그래밍 ch11

---
+++ lecture/fastcampus/java-spring/src/main/java/kr/co/fastcampus/javaspring/java/ch11/OperatorTest.java
@@ -0,0 +1,18 @@
+package kr.co.fastcampus.javaspring.java.ch11;
+
+public class OperatorTest {
+
+    public static void main(String[] args) {
+
+        int gameScore = 150;
+        int lastScore = ++gameScore;
+
+        System.out.println(lastScore);
+        System.out.println(gameScore);
+
+        lastScore = gameScore++;
+
+        System.out.println(lastScore);
+        System.out.println(gameScore);
+    }
+}
