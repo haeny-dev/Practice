217f0dc2f60cde296d811180046e9ea46de2faff
haeny-dev <wjdeoaks92@gmail.com>
2021-05-14 20:29:10 +0900

    [Today I Learn] 패스트캠퍼스 > 한번에 끝내는 Java/Spring 웹 개발 마스터 초격차 패키지 Online > 자바 프로그래밍 ch13

---
+++ lecture/fastcampus/java-spring/src/main/java/kr/co/fastcampus/javaspring/java/ch13/BitOperationTest.java
@@ -0,0 +1,21 @@
+package kr.co.fastcampus.javaspring.java.ch13;
+
+public class BitOperationTest {
+
+    public static void main(String[] args) {
+
+        int num1 = 5;
+        int num2 = 10;
+
+        System.out.println(num1 | num2);
+        System.out.println(num1 & num2);
+        System.out.println(num1 ^ num2);
+        System.out.println(~num1);
+
+        System.out.println(num1 << 2);
+        System.out.println(num1);
+
+        System.out.println(num1 <<= 2);
+        System.out.println(num1);
+    }
+}
