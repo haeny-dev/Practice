8139aef27b21bbf6c2c6cb784d37e97af33c8932
haeny-dev <wjdeoaks92@gmail.com>
2021-05-15 20:49:53 +0900

    [Today I Learn] 패스트캠퍼스 > 한번에 끝내는 Java/Spring 웹 개발 마스터 초격차 패키지 Online > 자바 프로그래밍 ch16

---
+++ lecture/fastcampus/java-spring/src/main/java/kr/co/fastcampus/javaspring/java/ch16/SwitchCaseTest.java
@@ -0,0 +1,47 @@
+package kr.co.fastcampus.javaspring.java.ch16;
+
+import java.util.Scanner;
+
+public class SwitchCaseTest {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int month = scanner.nextInt();
+
+        int day;
+        switch (month) {
+            case 1: case 3: case 5: case 7: case 8: case 10: case 11: case 12:
+                day = 31;
+                break;
+            case 2:
+                day = 28;
+                break;
+            case 4: case 6: case 9:
+                day = 30;
+                break;
+            default:
+                System.out.println("존재하지 않는 달 입니다.");
+                day = -1;
+        }
+
+        System.out.println(month + "월은 " + day + "일 입니다.");
+
+        // Java 14
+        day = switch (month){
+            case 1, 3, 5, 7, 8, 10, 12 ->
+                    31;
+            case 2 ->
+                    28;
+            case 4, 6, 9, 11 ->
+                    30;
+            default -> {
+                System.out.println("존재하지 않는 달 입니다.");
+                yield -1;
+            }
+        };
+
+        System.out.println(month + "월은 " + day + "일 입니다.");
+    }
+}
