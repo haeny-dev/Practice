6a06431b07977a0c5526dd83deed2f984cbf9782
haeny-dev <wjdeoaks92@gmail.com>
2021-05-19 18:53:56 +0900

    [Today I Learn] 프로그래머스 코딩테스트 연습 > 2021 카카오 채용연계형 인턴십 : 숫자 문자열과 영단어

---
+++ coding-test/programmers/src/kr/co/programmers/kakao_internship_2021/NumericStringsAndEnglishWords.java
@@ -0,0 +1,24 @@
+package kr.co.programmers.kakao_internship_2021;
+
+public class NumericStringsAndEnglishWords {
+
+    public static void main(String[] args) {
+        String s = "one4seveneight";
+        System.out.println(new Solution().solution(s));
+    }
+
+    static class Solution {
+        public int solution(String s) {
+            String[] words = {"zero","one","two","three","four","five","six","seven","eight","nine"};
+
+            for(int i = 0; i < words.length; i++){
+                if(s.contains(words[i])){
+                    s = s.replace(words[i], String.valueOf(i));
+                    continue;
+                }
+            }
+
+            return Integer.parseInt(s);
+        }
+    }
+}
