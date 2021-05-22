24f48513d423ae2382d3608076aabd1bfdda8657
haeny-dev <wjdeoaks92@gmail.com>
2021-05-22 19:22:11 +0900

    [Today I Learn] 프로그래머스 코딩테스트 연습 > 2018 KAKAO BLIND RECRUITMENT : 비밀지도

---
+++ coding-test/programmers/src/kr/co/programmers/kakao_blind_recruitment_2018/SecretMap.java
@@ -0,0 +1,41 @@
+package kr.co.programmers.kakao_blind_recruitment_2018;
+
+import java.util.Arrays;
+
+public class SecretMap {
+
+    public static void main(String[] args) {
+        int n = 5;
+        int[] arr1 = {9, 20, 28, 18, 11};
+        int[] arr2 = {30, 1, 21, 17, 28};
+        System.out.println(Arrays.toString(new Solution().solution(n, arr1, arr2)));
+    }
+
+    static class Solution {
+        public String[] solution(int n, int[] arr1, int[] arr2) {
+            String[] answer = new String[n];
+
+            for(int i = 0; i < n; i++){
+                String row = "";
+                int count = 0;
+                int mok1 = arr1[i];
+                int mok2 = arr2[i];
+
+                while(count < n){
+                    String temp = "#";
+                    if(mok1%2 == 0 && mok2%2 == 0){
+                        temp = " ";
+                    }
+
+                    row = temp + row;
+                    mok1 = mok1/2;
+                    mok2 = mok2/2;
+                    count++;
+                }
+                answer[i] = row;
+            }
+
+            return answer;
+        }
+    }
+}
