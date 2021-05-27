5ae9219e2cf31972a1463d6001997d6dd371c654
haeny-dev <wjdeoaks92@gmail.com>
2021-05-27 19:47:20 +0900

    [Today I Learn] 프로그래머스 코딩테스트 연습 > Summer/Winter Coding(2019) : 멀쩡한 사각형

---
+++ coding-test/programmers/src/kr/co/programmers/summer_winter_coding_2019/PlainSquare.java
@@ -0,0 +1,23 @@
+package kr.co.programmers.summer_winter_coding_2019;
+
+public class PlainSquare {
+
+    public static void main(String[] args) {
+        int w = 8;
+        int h = 12;
+        System.out.println(new Solution().solution(w, h));
+    }
+
+    static class Solution {
+        public long solution(int w, int h) {
+            long answer = 0;
+            double rate = (double) h / (double) w;
+
+            for(int i = 1; i <= w; i++){
+                answer += (long)(h - (i * rate)) * 2;
+            }
+
+            return answer;
+        }
+    }
+}
