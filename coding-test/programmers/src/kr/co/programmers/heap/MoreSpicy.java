aefa09c4d2e56bad397f451e876acd2a6ad06eb6
haeny-dev <wjdeoaks92@gmail.com>
2021-05-30 14:12:49 +0900

    [Today I Learn] 프로그래머스 코딩테스트 연습 > 힙(Heap) : 더 맵게

---
+++ coding-test/programmers/src/kr/co/programmers/heap/MoreSpicy.java
@@ -0,0 +1,39 @@
+package kr.co.programmers.heap;
+
+import java.util.PriorityQueue;
+
+public class MoreSpicy {
+
+    public static void main(String[] args) {
+        int[] scoville = {1, 2, 3, 9, 10, 12};
+        int K = 7;
+        System.out.println(new Solution().solution(scoville, K));
+    }
+
+    static class Solution {
+        public int solution(int[] scoville, int K) {
+            int answer = 0;
+
+            PriorityQueue<Integer> scovilleQue = new PriorityQueue<>();
+            for(int s : scoville){
+                scovilleQue.offer(s);
+            }
+
+            while(scovilleQue.peek() <= K){
+                if(scovilleQue.size() < 2){
+                    return -1;
+                }
+
+                int min = scovilleQue.poll();
+                int nextMin = scovilleQue.poll();
+                int newValue = min + nextMin * 2;
+
+                scovilleQue.offer(newValue);
+                answer++;
+            }
+
+            return answer;
+        }
+
+    }
+}
