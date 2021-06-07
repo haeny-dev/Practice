c9c81c0c6374f4e820d360e40b5b8c4d0cd0d71c
haeny-dev <wjdeoaks92@gmail.com>
2021-06-07 15:03:12 +0900

    [Today I Learn] 프로그래머스 코딩테스트 연습 > 정렬 : 가장 큰 수

---
+++ coding-test/programmers/src/kr/co/programmers/sort/BiggestNumber.java
@@ -0,0 +1,24 @@
+package kr.co.programmers.sort;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class BiggestNumber {
+
+    public static void main(String[] args) {
+        int[] numbers = {6, 10, 2};
+        System.out.println(new Solution().solution(numbers));
+    }
+
+    static class Solution {
+        public String solution(int[] numbers) {
+            String[] strings = new String[numbers.length];
+            for(int i = 0; i < numbers.length; i++){
+                strings[i] = String.valueOf(numbers[i]);
+            }
+
+            Arrays.sort(strings, (a, b) -> (b + a).compareTo(a + b));
+            return "0".equals(strings[0]) ? "0" : Arrays.stream(strings).collect(Collectors.joining());
+        }
+    }
+}
