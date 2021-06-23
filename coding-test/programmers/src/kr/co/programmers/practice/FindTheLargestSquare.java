f74dc9b41e8abc2db527363fe007ce25fcbbc61d
haeny-dev <wjdeoaks92@gmail.com>
2021-06-23 23:34:38 +0900

    [Today I Learn] 프로그래머스 코딩테스트 연습 > 연습문제 : 가장 큰 정사각형 찾기

---
+++ coding-test/programmers/src/kr/co/programmers/practice/FindTheLargestSquare.java
@@ -0,0 +1,36 @@
+package kr.co.programmers.practice;
+
+public class FindTheLargestSquare {
+
+    public static void main(String[] args) {
+        int[][] board = {{0, 1, 1, 1}, {1, 1, 1, 1}, {1, 1, 1, 1}, {0, 0, 1, 0}};
+        System.out.println(new Solution().solution(board));
+    }
+
+    static class Solution{
+        public int solution(int [][]board){
+            int answer = 0;
+
+            if(board.length == 1 || board[0].length == 1){
+                for(int i = 0; i < board.length; i++){
+                    for(int j = 0; i < board[0].length; j++){
+                        if(board[i][j] == 1){
+                            return 1;
+                        }
+                    }
+                }
+            }
+
+            for(int i = 1; i < board.length; i++){
+                for(int j = 1; j < board[0].length; j++){
+                    if(board[i][j] == 0) continue;
+
+                    board[i][j] = board[i][j] + Math.min(board[i-1][j] ,Math.min(board[i-1][j-1], board[i][j-1]));
+                    answer = answer > board[i][j] ? answer : board[i][j];
+                }
+            }
+
+            return answer * answer;
+        }
+    }
+}
