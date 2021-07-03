3b7997012c8f032913df849b8c0ef1d0d1a922b8
haeny-dev <wjdeoaks92@gmail.com>
2021-07-03 15:21:39 +0900

    [Today I Learn] 프로그래머스 코딩테스트 연습 > 연습문제 : 피보나치 수

---
+++ coding-test/programmers/src/kr/co/programmers/practice/FibonacciNumber.java
@@ -0,0 +1,24 @@
+package kr.co.programmers.practice;
+
+public class FibonacciNumber {
+
+    public static void main(String[] args) {
+        int n = 3;
+        System.out.println(new Solution().solution(n));
+    }
+
+    static class Solution {
+        public int solution(int n) {
+            int answer = 0;
+            int n_2 = 0;
+            int n_1 = 1;
+            for(int i = 0; i < n-1; i++){
+                answer = (n_1 + n_2) % 1234567;
+                n_2 = n_1;
+                n_1 = answer;
+            }
+
+            return answer;
+        }
+    }
+}
