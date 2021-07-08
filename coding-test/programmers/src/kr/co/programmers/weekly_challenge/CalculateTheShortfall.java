111b82460e04670a3e0305401fc77b86292a87b4
haeny-dev <wjdeoaks92@gmail.com>
2021-07-08 15:44:04 +0900

    [Today I Learn] 프로그래머스 코딩테스트 연습 > 위클리 챌린지 : 부족한 금액 계산하기

---
+++ coding-test/programmers/src/kr/co/programmers/weekly_challenge/CalculateTheShortfall.java
@@ -0,0 +1,23 @@
+package kr.co.programmers.weekly_challenge;
+
+public class CalculateTheShortfall {
+
+    public static void main(String[] args) {
+        int price = 3;
+        int money = 20;
+        int count = 4;
+        System.out.println(new Solution().solution(price, money, count));
+    }
+
+    static class Solution {
+        public long solution(int price, int money, int count) {
+            long answer = money;
+
+            for(int i = 0; i < count; i++){
+                answer -= price * (i+1);
+            }
+
+            return answer < 0 ? Math.abs(answer) : 0;
+        }
+    }
+}
