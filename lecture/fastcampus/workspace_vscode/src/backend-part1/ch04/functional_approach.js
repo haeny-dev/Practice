2810130613c92fb2e8bb7b034422b9411d75f0f5
haeny-dev <wjdeoaks92@gmail.com>
2021-09-04 17:12:51 +0900

    [Today I Learn] 패스트 캠퍼스 강의 백엔드 Part1 > Ch04.모던 JavaScript 살펴보기 > 04.functional approach(1)

---
+++ lecture/fastcampus/workspace_vscode/src/backend-part1/ch04/functional_approach.js
@@ -0,0 +1,76 @@
+// @ts-check
+/* eslint-disable no-restricted-syntax */
+
+/**
+ * @typedef Person
+ *
+ * @property {number} age
+ * @property {string} city
+ * @property {string | string[]} [pet]
+ */
+
+/** @type {Person[]} */
+const people = [
+  {
+    age: 20,
+    city: '서울',
+    pet: ['cat', 'dog'],
+  },
+  {
+    age: 40,
+    city: '부산',
+  },
+  {
+    age: 31,
+    city: '대구',
+    pet: ['cat', 'dog'],
+  },
+  {
+    age: 36,
+    city: '서울',
+  },
+  {
+    age: 27,
+    city: '부산',
+    pet: 'cat',
+  },
+  {
+    age: 24,
+    city: '서울',
+    pet: 'dog',
+  },
+]
+
+/**
+ * Quiz
+ *
+ * A. 30대 미만이 한 명이라도 사는 모든 도시
+ * B. 각 도시별로 개와 고양이를 키우는 사람의 수
+ */
+
+// A
+function solveA() {
+  /** @type {string[]} */
+  const cities = []
+
+  for (const person of people) {
+    if (person.age < 30) {
+      if (!cities.find((city) => person.city === city)) {
+        cities.push(person.city)
+      }
+    }
+  }
+
+  return cities
+}
+
+function solveAModern() {
+  const allCities = people
+    .filter((person) => person.age < 30)
+    .map((person) => person.city)
+  const set = new Set(allCities)
+  return Array.from(set)
+}
+
+console.log('solveA', solveA())
+console.log('solveAModern', solveAModern())
