aa9e289499def6f22b0d62cc3797036aaaf02ee5
haeny-dev <wjdeoaks92@gmail.com>
2021-12-11 21:51:12 +0900

    [Today I Learn] 패스트 캠퍼스 강의 백엔드 Part1 > Ch08.리팩토링 프로젝트 > 03.async, await 등 모던 패턴을 적용해 개선하기(1)

--- lecture/fastcampus/workspace_vscode/backend-part1/ch08/src/after.js
+++ lecture/fastcampus/workspace_vscode/backend-part1/ch08/src/after.js
@@ -1,5 +1,6 @@
 // @ts-check
 
+const { rejects } = require('assert')
 const https = require('https')
 
 /**
@@ -16,26 +17,65 @@ const https = require('https')
 const GOTAPI_PREFIX = 'https://game-of-thrones-quotes.herokuapp.com/v1'
 
 /**
- * @returns {Promise<House[]>}
+ * @param {string} url
+ * @returns {*}
  */
-async function getHouses() {
-  return new Promise((resolve) => {
-    https.get(`${GOTAPI_PREFIX}/houses`, (res) => {
+async function getHttpsJson(url) {
+  return new Promise((resolve, reject) => {
+    https.get(url, (res) => {
       let jsonStr = ''
       res.setEncoding('utf-8')
       res.on('data', (data) => {
         jsonStr += data
       })
       res.on('end', () => {
-        resolve(JSON.parse(jsonStr))
+        try {
+          const parsed = JSON.parse(jsonStr)
+          resolve(parsed)
+        } catch {
+          reject(
+            new Error('The server response was not a valid JSON document.')
+          )
+        }
       })
     })
   })
 }
 
+/**
+ * @returns {Promise<House[]>}
+ */
+async function getHouses() {
+  return getHttpsJson(`${GOTAPI_PREFIX}/houses`)
+}
+
+/**
+ * 
+ * @param {string} quote 
+ * @returns {string}
+ */
+function sanitizeQuote(quote){
+  return quote.replace(/[^a-zA-Z0-9., ]/g, '')
+}
+
+/**
+ *
+ * @param {string} slug
+ * @returns {Promise<string>}
+ */
+async function getMergedQuotesOfCharacter(slug) {
+  const character = await getHttpsJson(`${GOTAPI_PREFIX}/character/${slug}`)
+  return sanitizeQuote(character[0].quotes.join(' '))
+
 async function main() {
   const houses = await getHouses()
-  console.log(houses)
+  houses.forEach((house) => {
+    house.members.forEach((member) => {
+      getMergedQuotesOfCharacter(member.slug).then((quotes) =>
+        console.log(house.slug, member.slug, quotes)
+      )
+    })
+  })
 }
 
 main()
