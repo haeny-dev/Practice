1562c675d57021c4b07b05b1d3c22107be9147b5
haeny-dev <wjdeoaks92@gmail.com>
2021-12-13 19:50:32 +0900

    [Today I Learn] 패스트 캠퍼스 강의 백엔드 Part2 > 11.웹소켓을 통한 실시간 인터렉션 구현 > 03.웹소켓 미들웨어 사용해 프론트엔드와 연결하기

--- lecture/fastcampus/workspace_vscode/backend-part1/ch11/src/main.js
+++ lecture/fastcampus/workspace_vscode/backend-part1/ch11/src/main.js
@@ -6,8 +6,12 @@
 const Koa = require('koa')
 const Pug = require('koa-pug')
 const path = require('path')
+const route = require('koa-route')
+const serve = require('koa-static')
+const mount = require('koa-mount')
+const websockify = require('koa-websocket')
 
-const app = new Koa()
+const app = websockify(new Koa())
 
 // @ts-ignore
 // eslint-disable-next-line no-new
@@ -16,8 +20,42 @@ new Pug({
   app,
 })
 
+app.use(mount('/public', serve('src/public')))
+
 app.use(async (ctx) => {
   await ctx.render('main')
 })
+
+// Using routes
+app.ws.use(
+  route.all('/ws', (ctx) => {
+    // `ctx` is the regular koa context created from the `ws` onConnection `socket.upgradeReq` object.
+    // the websocket is added to the context on `ctx.websocket`.
+    ctx.websocket.send('Hello World')
+    ctx.websocket.on('message', (data) => {
+      // do something with the message from client
+      if (typeof data !== 'string') {
+        return
+      }
+
+      const { message, nickname } = JSON.parse(data)
+
+      const { server } = app.ws
+
+      if (!server) {
+        return
+      }
+
+      server.clients.forEach((client) => {
+        client.send(
+          JSON.stringify({
+            message,
+            nickname,
+          })
+        )
+      })
+    })
+  })
+)
 
 app.listen(4000)
