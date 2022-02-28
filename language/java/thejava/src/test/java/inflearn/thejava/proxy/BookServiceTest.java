2af05dab63c34314de2ef3f0db6bbcef1a00632a
haeny-dev <wjdeoaks92@gmail.com>
2022-02-28 18:04:13 +0900

    [The Java] 다이나믹 프록시 > 클래스의 프록시가 필요하다면? CGLIB, ByteBuddy

--- language/java/thejava/src/test/java/inflearn/thejava/proxy/BookServiceTest.java
+++ language/java/thejava/src/test/java/inflearn/thejava/proxy/BookServiceTest.java
@@ -1,15 +1,23 @@
 package inflearn.thejava.proxy;
 
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.implementation.InvocationHandlerAdapter;
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
+import static net.bytebuddy.matcher.ElementMatchers.named;
+
 class BookServiceTest {
 
     // Class 기반의 다이나믹 프록시는 생성할 수 없다.
     // Interface 기반의 다이나믹 프록시만 생성할 수 있다.
+    /*
     BookService bookService = (BookService) Proxy.newProxyInstance(
             BookService.class.getClassLoader(),
             new Class[]{BookService.class},
@@ -28,9 +36,49 @@ class BookServiceTest {
                     return method.invoke(bookService, args);
                 }
             });
+     */
 
     @Test
-    void rent() {
+    void rent() throws Exception {
+        /* CGLIB 사용
+        MethodInterceptor handler = new MethodInterceptor() {
+            DefaultBookService bookService = new DefaultBookService();
+
+            @Override
+            public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+                if (method.getName().equals("rent")) {
+                    System.out.println("=== preMethod");
+                    Object invoke = method.invoke(bookService, args);
+                    System.out.println("=== postMethod");
+                    return invoke;
+                }
+
+                return method.invoke(bookService, args);
+            }
+        };
+        BookService bookService = (BookService) Enhancer.create(BookService.class, handler);
+         */
+
+        /* ByteBuddy 사용 */
+        Class<? extends DefaultBookService> proxyClass = new ByteBuddy().subclass(DefaultBookService.class)
+                .method(named("rent")).intercept(InvocationHandlerAdapter.of(new InvocationHandler() {
+                    DefaultBookService bookService = new DefaultBookService();
+                    @Override
+                    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                        if (method.getName().equals("rent")) {
+                            System.out.println("=== preMethod");
+                            Object invoke = method.invoke(bookService, args);
+                            System.out.println("=== postMethod");
+                            return invoke;
+                        }
+
+                        return method.invoke(bookService, args);
+                    }
+                }))
+                .make().load(DefaultBookService.class.getClassLoader()).getLoaded();
+
+        DefaultBookService bookService = proxyClass.getConstructor(null).newInstance();
+
         Book book = new Book();
         book.setTitle("spring");
         bookService.rent(book);
