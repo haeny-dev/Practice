c1b8e6f6c1fd6c8e0d0973118a51f3830f57392a
haeny-dev <wjdeoaks92@gmail.com>
2022-03-02 15:17:03 +0900

    [TIL] 백준 10926번

---
+++ baekjoon/src/net/acmicpc/step/problem10926/Main.java
@@ -0,0 +1,12 @@
+package net.acmicpc.step.problem10926;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print(br.readLine() + "??!");
+    }
+}
