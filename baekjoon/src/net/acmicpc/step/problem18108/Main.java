178ca3b33ab7831a952490f5a5df7077d2d9fcc9
haeny-dev <wjdeoaks92@gmail.com>
2022-03-02 15:20:39 +0900

    [TIL] 백준 18108번

---
+++ baekjoon/src/net/acmicpc/step/problem18108/Main.java
@@ -0,0 +1,13 @@
+package net.acmicpc.step.problem18108;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        final int difference = 2541 - 1998;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print(Integer.parseInt(br.readLine()) - difference);
+    }
+}
