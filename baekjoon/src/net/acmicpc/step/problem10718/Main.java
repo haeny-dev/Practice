f59fb389a7a7e73502c023e1790cf7569246b9de
haeny-dev <wjdeoaks92@gmail.com>
2022-03-02 14:25:47 +0900

    [TIL] 백준 10718번

---
+++ baekjoon/src/net/acmicpc/step/problem10718/Main.java
@@ -0,0 +1,16 @@
+package net.acmicpc.step.problem10718;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) {
+        try(BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))){
+            bw.write("강한친구 대한육군\n강한친구 대한육군");
+            bw.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
