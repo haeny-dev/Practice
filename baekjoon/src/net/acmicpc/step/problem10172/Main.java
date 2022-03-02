75f73877e8dbc63abc3e342b06efcc1071d1eae2
haeny-dev <wjdeoaks92@gmail.com>
2022-03-02 14:34:42 +0900

    [TIL] 백준 10172번

---
+++ baekjoon/src/net/acmicpc/step/problem10172/Main.java
@@ -0,0 +1,20 @@
+package net.acmicpc.step.problem10172;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) {
+        try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            bw.write("|\\_/|\n");
+            bw.write("|q p|   /}\n");
+            bw.write("( 0 )\"\"\"\\\n");
+            bw.write("|\"^\"`    |\n");
+            bw.write("||_/=\\\\__|\n");
+            bw.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
