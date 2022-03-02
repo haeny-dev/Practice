8b4dee0357614e647cea1c9079bbdf69f759a785
haeny-dev <wjdeoaks92@gmail.com>
2022-03-02 14:51:30 +0900

    [TIL] 백준 1000번

---
+++ baekjoon/src/net/acmicpc/step/problem1000/Main.java
@@ -0,0 +1,25 @@
+package net.acmicpc.step.problem1000;
+
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            bw.write(String.valueOf(Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).sum()));
+            bw.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+        try {
+            int a = System.in.read() - 48;
+            System.in.read();
+            System.out.print(a + System.in.read() - 48);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
