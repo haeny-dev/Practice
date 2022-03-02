fe7d602ffe3c6bf9dc18b49890d3ac02acf4f92d
haeny-dev <wjdeoaks92@gmail.com>
2022-03-02 14:21:20 +0900

    [TIL] 백준 2557번

---
+++ baekjoon/src/net/acmicpc/step/problem2557/Main.java
@@ -0,0 +1,13 @@
+package net.acmicpc.step.problem2557;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        //System.out.print("Hello World!");
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+        bufferedWriter.write("Hello World!");
+        bufferedWriter.flush();
+        bufferedWriter.close();
+    }
+}
--- language/java/thejava/src/test/java/inflearn/thejava/annotationprocessor/MemberTest.java
+++ language/java/thejava/src/test/java/inflearn/thejava/annotationprocessor/MemberTest.java
@@ -1,10 +1,8 @@
 package inflearn.thejava.annotationprocessor;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class MemberTest {
 
