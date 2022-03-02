ad3a71ed5c22396d7ab0737dec108dff0f1fdb28
haeny-dev <wjdeoaks92@gmail.com>
2022-03-02 15:41:18 +0900

    [TIL] 백준 2588번

---
+++ baekjoon/src/net/acmicpc/step/problem2588/Main.java
@@ -0,0 +1,19 @@
+package net.acmicpc.step.problem2588;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        String bLine = br.readLine();
+        int b = Integer.parseInt(bLine);
+        String[] split = bLine.split("");
+        System.out.println(a * Integer.parseInt(split[2]));
+        System.out.println(a * Integer.parseInt(split[1]));
+        System.out.println(a * Integer.parseInt(split[0]));
+        System.out.print(a * b);
+    }
+}
