7d1adae46ad29e4c98335b3ef6abc243c2cfe5e5
haeny-dev <wjdeoaks92@gmail.com>
2022-03-03 13:10:36 +0900

    [TIL] 백준 9498번

---
+++ baekjoon/src/net/acmicpc/step/ifstatement/problem9498/Main.java
@@ -0,0 +1,23 @@
+package net.acmicpc.step.ifstatement.problem9498;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        if (a >= 90) {
+            System.out.print("A");
+        } else if (a >= 80) {
+            System.out.print("B");
+        } else if (a >= 70) {
+            System.out.print("C");
+        } else if (a >= 60) {
+            System.out.print("D");
+        } else {
+            System.out.print("F");
+        }
+    }
+}
