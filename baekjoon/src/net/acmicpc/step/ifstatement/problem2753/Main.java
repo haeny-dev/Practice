13501e95128705608b0e4f9e2d0b7bad3761618c
haeny-dev <wjdeoaks92@gmail.com>
2022-03-03 13:18:34 +0900

    [TIL] 백준 2753번

---
+++ baekjoon/src/net/acmicpc/step/ifstatement/problem2753/Main.java
@@ -0,0 +1,13 @@
+package net.acmicpc.step.ifstatement.problem2753;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int year = Integer.parseInt(br.readLine());
+        System.out.print((year % 4 == 0 && year % 100 != 0) || year % 400 == 0 ? 1 : 0);
+    }
+}
