ddab87d70914a06a47687c65d7fc12981a114ada
haeny-dev <wjdeoaks92@gmail.com>
2022-03-03 13:37:04 +0900

    [TIL] 백준 2884번

---
+++ baekjoon/src/net/acmicpc/step/ifstatement/problem2884/Main.java
@@ -0,0 +1,22 @@
+package net.acmicpc.step.ifstatement.problem2884;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int h = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        if (m < 45) {
+            h = h == 0 ? 24 - 1 : h - 1;
+            m += 15;
+        } else {
+            m -= 45;
+        }
+        System.out.print(h + " " + m);
+    }
+}
