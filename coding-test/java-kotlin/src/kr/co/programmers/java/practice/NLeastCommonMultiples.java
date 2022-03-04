803c3e32483a2d82bf7391e943564c1d4e8d5e33
haeny-dev <wjdeoaks92@gmail.com>
2022-03-04 16:43:20 +0900

    TIL: programmers 연습문제: N개의 최소공배수 다시 풀기

--- coding-test/java-kotlin/src/kr/co/programmers/java/practice/NLeastCommonMultiples.java
+++ coding-test/java-kotlin/src/kr/co/programmers/java/practice/NLeastCommonMultiples.java
@@ -6,28 +6,46 @@ public class NLeastCommonMultiples {
 
     public static void main(String[] args) {
         int[] arr = {2, 6, 8, 14};
+//        System.out.println(new Solution().solution(arr));
         System.out.println(new Solution().solution(arr));
     }
 
+
     static class Solution {
+        public int diffSolution(int[] arr) {
+            int answer = arr[0];
+            for (int i = 1, g; i < arr.length; i++) {
+                g = gcd(answer, arr[i]);
+                answer = (answer * arr[i]) / g;
+            }
+            return answer;
+        }
+
+        public int gcd(int a, int b) {
+            if (a > b)
+                return (a % b == 0) ? b : gcd(b, a % b);
+            else
+                return (b % a == 0) ? a : gcd(a, b % a);
+        }
+
         public int solution(int[] arr) {
             int answer = 0;
 
             Arrays.sort(arr);
-            int max = arr[arr.length-1];
+            int max = arr[arr.length - 1];
             int multiple = 1;
 
-            while(answer == 0){
+            while (answer == 0) {
                 int temp = max * multiple;
 
-                for(int i = 0; i < arr.length; i++){
-                    if(temp % arr[i] != 0){
+                for (int i = 0; i < arr.length; i++) {
+                    if (temp % arr[i] != 0) {
                         temp = 0;
                         break;
                     }
                 }
 
-                if(temp == 0){
+                if (temp == 0) {
                     multiple++;
                     continue;
                 }
