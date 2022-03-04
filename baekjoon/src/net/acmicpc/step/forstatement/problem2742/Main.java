8fcf35b4bc0913f0802d6286c9c5c020a93f5a43
haeny-dev <wjdeoaks92@gmail.com>
2022-03-04 14:14:17 +0900

    TIL: baekjoon problem 2742

---
+++ baekjoon/src/net/acmicpc/step/forstatement/problem2742/Main.java
@@ -0,0 +1,17 @@
+package net.acmicpc.step.forstatement.problem2742;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        for (int i = n; i > 0; i--) {
+            bw.write(i + "\n");
+        }
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
