cbc9f5498436fe047a1e6fd94a4c7989114faa2a
haeny-dev <wjdeoaks92@gmail.com>
2022-03-06 10:29:56 +0900

    [TIL] java final keyword

---
+++ language/java/practice/src/finalkeyword/FinalKeyworkTest.java
@@ -0,0 +1,8 @@
+package finalkeyword;
+
+public class FinalKeyworkTest {
+    public static void main(String[] args) {
+        final int finalNumber = 1;  // final 필드
+//        finalNumber = 2;    // 컴파일 에러
+    }
+}
---
+++ language/java/practice/src/finalkeyword/MemberService.java
@@ -0,0 +1,11 @@
+package finalkeyword;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+}
