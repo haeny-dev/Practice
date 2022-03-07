a05a020166fc573274ded63540579f456c503dfd
haeny-dev <wjdeoaks92@gmail.com>
2022-03-07 13:56:33 +0900

    TIL: programmers 연습문제> 행렬의 곱셈

--- coding-test/java-kotlin/src/kr/co/programmers/java/practice/MatrixMultiplication.java
+++ coding-test/java-kotlin/src/kr/co/programmers/java/practice/MatrixMultiplication.java
@@ -5,9 +5,14 @@ import java.util.Arrays;
 public class MatrixMultiplication {
 
     public static void main(String[] args) {
-        int[][] arr1 = {{1, 4}, {3, 2}, {4, 1}};
-        int[][] arr2 = {{3, 3}, {3, 3}};
-        Arrays.stream(new Solution().solution(arr1, arr2)).forEach(x -> System.out.println(Arrays.toString(x)));
+        int[][] arr1 = {
+                {1, 4},
+                {3, 2},
+                {4, 1}};    // 3 x 2
+        int[][] arr2 = {
+                {3, 3},
+                {3, 3}};    // 2 x 2
+        Arrays.stream(new Solution().solution2(arr1, arr2)).forEach(x -> System.out.println(Arrays.toString(x)));
     }
 
     static class Solution {
@@ -17,6 +22,20 @@ public class MatrixMultiplication {
             for(int i = 0; i < arr1.length; i++){
                 for(int j = 0; j < arr2[0].length; j++){
                     for(int k = 0; k < arr2.length; k++){
+                        answer[i][j] += arr1[i][k] * arr2[k][j];
+                    }
+                }
+            }
+
+            return answer;
+        }
+
+        public int[][] solution2(int[][] arr1, int[][] arr2){
+            int[][] answer = new int[arr1.length][arr2[0].length];
+
+            for (int i = 0; i < arr1.length; i++) {
+                for (int j = 0; j < arr2[0].length; j++) {
+                    for (int k = 0; k < arr1[0].length; k++) {
                         answer[i][j] += arr1[i][k] * arr2[k][j];
                     }
                 }
