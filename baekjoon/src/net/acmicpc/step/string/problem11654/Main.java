a8aec921034f2f4e5f88580f57d1969a8df2d1f7
haeny-dev <wjdeoaks92@gmail.com>
2022-03-08 18:30:57 +0900

    TIL: baekjoon problem 11654

---
+++ baekjoon/src/net/acmicpc/step/string/problem11654/Main.java
@@ -0,0 +1,11 @@
+package net.acmicpc.step.string.problem11654;
+
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.print(System.in.read());
+    }
+}
