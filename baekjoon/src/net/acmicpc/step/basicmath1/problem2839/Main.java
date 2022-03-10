077a368b4950414741994d0c72da67d1d4ea9c70
haeny-dev <wjdeoaks92@gmail.com>
2022-03-10 17:18:29 +0900

    TIL: baekjoon problem 2839

---
+++ baekjoon/src/net/acmicpc/step/basicmath1/problem2839/Main.java
@@ -0,0 +1,40 @@
+package net.acmicpc.step.basicmath1.problem2839;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int N = readInt();
+
+        if (N == 4 || N == 7) {
+            System.out.print(-1);
+        }
+
+        if (N % 5 == 0) {
+            System.out.print(N / 5);
+        }
+
+        if (N % 5 == 1 || N % 5 == 3) {
+            System.out.print(N / 5 + 1);
+        }
+
+        if (N % 5 == 2 || N % 5 == 4) {
+            System.out.print(N / 5 + 2);
+        }
+    }
+
+    static int readInt() throws IOException {
+        boolean isNegative = false;
+        int sum = 0;
+        while (true) {
+            int input = System.in.read();
+            if (input == '\n' || input == ' ') {
+                return isNegative ? -1 * sum : sum;
+            } else if (input == '-') {
+                isNegative = true;
+            } else {
+                sum = (sum * 10) + (input - '0');
+            }
+        }
+    }
+}
