0bec9176a62bdd23e4baa1f6354ee4294bc29257
haeny-dev <wjdeoaks92@gmail.com>
2022-03-13 18:46:33 +0900

    TIL: baekjoon problem 3009

---
+++ baekjoon/src/net/acmicpc/step/basicmath2/problem3009/Main.java
@@ -0,0 +1,44 @@
+package net.acmicpc.step.basicmath2.problem3009;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int[] x = new int[3];
+        int[] y = new int[3];
+
+        for (int i = 0; i < 3; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            x[i] = Integer.parseInt(st.nextToken());
+            y[i] = Integer.parseInt(st.nextToken());
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        if (x[0] == x[1]) {
+            sb.append(x[2]);
+        } else if (x[0] == x[2]) {
+            sb.append(x[1]);
+        } else {
+            sb.append(x[0]);
+        }
+
+        sb.append(" ");
+
+        if (y[0] == y[1]) {
+            sb.append(y[2]);
+        } else if (y[0] == y[2]) {
+            sb.append(y[1]);
+        } else {
+            sb.append(y[0]);
+        }
+
+        System.out.print(sb);
+    }
+}
