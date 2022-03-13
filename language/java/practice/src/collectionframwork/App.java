239bd761d9ad471e7536e85b7d6609e965c9b739
haeny-dev <wjdeoaks92@gmail.com>
2022-03-13 17:59:33 +0900

    TIL: study java collection framework

--- language/java/practice/src/collectionframwork/App.java
+++ language/java/practice/src/collectionframwork/App.java
@@ -1,12 +1,29 @@
 package collectionframwork;
 
 import java.util.HashSet;
+import java.util.IdentityHashMap;
+import java.util.LinkedHashMap;
 import java.util.Set;
 
 public class App {
 
-
     public static void main(String[] args) {
+    }
+
+    private static void linkedHashMap() {
+        LinkedHashMap<String, String> linkedHashMap = new LinkedHashMap<>();
+        linkedHashMap.put("a", "valueA");
+        linkedHashMap.put("b", "valueB");
+        linkedHashMap.put("c", "valueC");
+
+        linkedHashMap.forEach((key, value) -> System.out.println(key));
+
+        linkedHashMap.put("a", "newValue");
+
+        linkedHashMap.forEach((key, value) -> System.out.println(key));
+    }
+
+    private static void set() {
         Set<String> set1 = new HashSet<>();
         Set<String> set2 = new HashSet<>();
         set1.add("String");
