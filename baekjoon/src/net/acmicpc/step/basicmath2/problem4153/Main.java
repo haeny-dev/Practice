13f39ee59c4294ed634493f35cf22482c83eab15
haeny-dev <wjdeoaks92@gmail.com>
2022-03-14 17:17:44 +0900

    TIL: baekjoon problem 4153

---
+++ baekjoon/src/net/acmicpc/step/basicmath2/problem4153/Main.java
@@ -0,0 +1,38 @@
+package net.acmicpc.step.basicmath2.problem4153;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            st = new StringTokenizer(br.readLine(), " ");
+
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int z = Integer.parseInt(st.nextToken());
+
+            if (x == 0 && y == 0 && z == 0) {
+                break;
+            }
+
+            if (x * x == y * y + z * z) {
+                sb.append("right").append("\n");
+            } else if (y * y == x * x + z * z) {
+                sb.append("right").append("\n");
+            } else if (z * z == x * x + y * y) {
+                sb.append("right").append("\n");
+            } else {
+                sb.append("wrong").append("\n");
+            }
+        }
+
+        System.out.print(sb);
+    }
+}
