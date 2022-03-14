337739c036f51ce634c9147bbc9ce4a1b8730940
haeny-dev <wjdeoaks92@gmail.com>
2022-03-14 16:50:50 +0900

    TIL: study thymeleaf attribute, condition, javascript

--- thymeleaf/src/main/java/com/thymeleaf/basic/BasicController.java
+++ thymeleaf/src/main/java/com/thymeleaf/basic/BasicController.java
@@ -58,4 +58,31 @@ public class BasicController {
         model.addAttribute("data", "Thymeleaf!");
         return "basic/operation";
     }
+
+    @GetMapping("/attribute")
+    public String attribute() {
+        return "basic/attribute";
+    }
+
+    @GetMapping("/condition")
+    public String condition(Model model) {
+        addUsers(model);
+        return "basic/condition";
+    }
+
+    @GetMapping("/javascript")
+    public String javascript(Model model) {
+        model.addAttribute("user", new User("userA", 10));
+        return "basic/javascript";
+    }
+
+    private void addUsers(Model model) {
+        List<User> list = new ArrayList<>();
+        list.add(new User("userA", 10));
+        list.add(new User("userB", 20));
+        list.add(new User("userC", 30));
+        model.addAttribute("users", list);
+    }
+
+
 }
