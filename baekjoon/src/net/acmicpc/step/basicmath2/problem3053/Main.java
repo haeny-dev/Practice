e1c23db2b66b7c753edf37ab343f611801395456
haeny-dev <wjdeoaks92@gmail.com>
2022-03-14 17:27:06 +0900

    TIL: baekjoon problem 3053

---
+++ baekjoon/src/net/acmicpc/step/basicmath2/problem3053/Main.java
@@ -0,0 +1,14 @@
+package net.acmicpc.step.basicmath2.problem3053;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double R = in.nextDouble();
+        in.close();
+
+        System.out.println(Math.PI * R * R);
+        System.out.println(2 * R * R);
+    }
+}
