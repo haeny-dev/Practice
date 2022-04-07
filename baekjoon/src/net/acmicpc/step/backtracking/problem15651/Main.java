bdb43c87b28852180120d51811de96b1898c396e
haeny-dev <wjdeoaks92@gmail.com>
2022-04-07 18:36:24 +0900

    TIL: baekjoon problem 15651

---
+++ baekjoon/src/net/acmicpc/step/backtracking/problem15651/Main.java
@@ -0,0 +1,40 @@
+package net.acmicpc.step.backtracking.problem15651;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static int[] arr;
+    private static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+        dfs(N, M, 0);
+
+        System.out.print(sb);
+    }
+
+    private static void dfs(int N, int M, int depth) {
+        if (depth == M) {
+            for (int v : arr) {
+                sb.append(v).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            arr[depth] = i;
+            dfs(N, M, depth + 1);
+        }
+    }
+}
