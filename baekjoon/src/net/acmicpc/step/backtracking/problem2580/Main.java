39a3b9651574b8b6ae5622205577104f166a4d65
haeny-dev <wjdeoaks92@gmail.com>
2022-04-11 19:30:38 +0900

    TIL: baekjoon problem 2580

---
+++ baekjoon/src/net/acmicpc/step/backtracking/problem2580/Main.java
@@ -0,0 +1,87 @@
+package net.acmicpc.step.backtracking.problem2580;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static int[][] arr = new int[9][9];
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int i = 0; i < 9; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < 9; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        sudoku(0, 0);
+
+    }
+
+    private static void sudoku(int row, int col) {
+
+        if (col == 9) {
+            sudoku(row + 1, 0);
+            return;
+        }
+
+        if (row == 9) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    sb.append(arr[i][j]).append(" ");
+                }
+                sb.append("\n");
+            }
+            System.out.print(sb);
+            System.exit(0);
+        }
+
+        if (arr[row][col] == 0) {
+            for (int i = 1; i <= 9; i++) {
+                if (checkValue(row, col, i)) {
+                    arr[row][col] = i;
+                    sudoku(row, col + 1);
+                }
+            }
+            arr[row][col] = 0;
+            return;
+        }
+
+        sudoku(row, col + 1);
+    }
+
+    private static boolean checkValue(int row, int col, int value) {
+
+        for (int i = 0; i < 9; i++) {
+            if (arr[row][i] == value) {
+                return false;
+            }
+        }
+
+        for (int i = 0; i < 9; i++) {
+            if (arr[i][col] == value) {
+                return false;
+            }
+        }
+
+        int startRow = (row / 3) * 3;
+        int startCol = (col / 3) * 3;
+
+        for (int i = startRow; i < startRow + 3; i++) {
+            for (int j = startCol; j < startCol + 3; j++) {
+                if (arr[i][j] == value) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
