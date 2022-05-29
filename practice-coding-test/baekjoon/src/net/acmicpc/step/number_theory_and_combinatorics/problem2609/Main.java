12056d68f74774183872101a35f500542be8671f
haeny-dev <wjdeoaks92@gmail.com>
2022-05-29 15:12:38 +0900

    TIL: baekjoon problem 2609

---
+++ practice-coding-test/baekjoon/src/net/acmicpc/step/number_theory_and_combinatorics/problem2609/Main.java
@@ -0,0 +1,27 @@
+package net.acmicpc.step.number_theory_and_combinatorics.problem2609;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        System.out.println(gcd(a, b));
+        System.out.println(a * b / gcd(a, b));
+    }
+
+    private static int gcd(int a, int b) {
+        if (a > b) {
+            return a % b == 0 ? b : gcd(b, a % b);
+        } else {
+            return b % a == 0 ? a : gcd(a, b % a);
+        }
+    }
+}
